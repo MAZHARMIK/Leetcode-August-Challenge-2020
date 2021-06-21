116d2df852312a377f5038211ddc76dac15910c6
MAZHAR <iiestshibpur605@gmail.com>
2021-06-21 21:17:42 +0530

    Added 2 approaches

--- Reorder List.cpp
+++ Reorder List.cpp
@@ -1,6 +1,36 @@
 /*
       Company Tags : Amazon, Microsoft, OYO Rooms
 */
+
+//Approach-1  (Using O(n) space)
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        stack<ListNode*> st;
+        
+        ListNode* curr = head;
+        while(curr) {
+            st.push(curr);
+            curr = curr->next;
+        }
+        
+        int k = st.size()/2;
+        curr = head;
+        while(k--) {
+            ListNode* topNode = st.top();
+            st.pop();
+            
+            ListNode* temp = curr->next;
+            curr->next = topNode;
+            topNode->next = temp;
+            curr = temp;
+        }
+        
+        curr->next = NULL;
+    }
+};
+
+//Approach-2 (Using O(1) space)
 class Solution {
 public:
     ListNode* reverseLL(ListNode* node) {
